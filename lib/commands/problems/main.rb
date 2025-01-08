91493b97cedd4ea9fce2d057d9429dbfcc0f4489
Load A <143745153+load-a@users.noreply.github.com>
2025-01-07 21:57:26 -0500

    Added more Problem functionality and documenting comments

--- lib/commands/problems/main.rb
+++ lib/commands/problems/main.rb
@@ -7,7 +7,8 @@ class Problems < Command
     addition: '+',
     subtraction: '-',
     multiplication: '*',
-    division: '/'
+    division: '/',
+    modulo: '%'
   }
 
   def initialize(argv)
@@ -21,11 +22,11 @@ class Problems < Command
 
     self.settings = {
       default_mode: :addition,
-      digits: 1,
-      lower_digits: 0,
-      questions: 10,
-      answers: true,
-      negatives: false,
+      digits: 1, # The primary number of digits
+      operator_digits: 0, # The number of digits the operator has; if left at zero it will match :digits
+      questions: 10, # The number of problems to generate
+      answers: true, # Whether answers are included with the questions
+      negative: 0, # the percentage of randomly generated negative operands. 
     }
 
     self.adjustments = {
@@ -33,26 +34,71 @@ class Problems < Command
       subtraction: {},
       multiplication: {},
       division: {
-        modulo: false,
-        float: false
-      }
+        modulo: false, # division returns only the remainder (normally it returns the quotient and remainder by default)
+        float: false # Whether divisions return floating point numbers
+      },
+      mixed: {}
     }
 
     self.directives = {
-      case_sensitivity: [false],
+      case_sensitivity: [:settings],
     }
 
     super
   end
 
+  # Generates a math problem using the configured settings.
+  # @return [String]
   def generate_problem
-    operand1 = number_in_range(self[:digits])
-    operand2 = number_in_range(self[:lower_digits])
+    operand = number_in_range(self[:digits])
+    operator = number_in_range(self[:operator_digits])
     operation = OPERATIONS[self[:active_mode]]
 
-    puts "#{operand1} #{operation} #{operand2} = #{operand1.send operation, operand2}"
+    # Returns a blank space if the number is not negative.
+    # @param number [Numeric]
+    # @return [String]
+    def spaced(number)
+      number.negative? ? '' : ' '
+    end
+
+    # Creates a string containing the number and a leading empty space if it is not negative.
+    # @param number [Numeric]
+    # @return [String]
+    def formatted(number)
+      "#{spaced(number)}#{number}"
+    end
+
+    problem = "#{formatted(operand)} #{operation} #{formatted(operator)} "
+    
+    return problem unless self[:answers]
+
+    # An auxiliary method for the result.
+    # It generates a formatted string containing the remainder of a division unless the :float setting is set.
+    # @param dividend [Numeric] The number being divided
+    # @param divisor [Numeric] The number doing the division
+    def remainder(dividend, divisor)
+      return if self[:float] == true
+
+      "r#{dividend % divisor}"
+    end
+
+    result = operand.send(operation, operator)
+    result_string = "= #{formatted(result)} #{remainder(operand, operator) if operation == '/'}"
+
+    problem + result_string
   end
 
+
+
+  # Generates a number within the given number of digits. 
+  # It generates random numbers between the lowest and highest possible values with that number of digits.
+  # (i.e. for 4 digits it will choose between 1000 and 9999).
+  # If the :float setting is set, the number returned will be a Float.
+  # It has :negative setting percent chance of generating a negative number.
+  # (i.e. :negative = 10, 10% chance the number is negative.)
+  # It will never generate a zero.
+  # @param digits [Integer]
+  # @return [Integer, Float]
   def number_in_range(digits)
     digits = 1 unless digits.is_a? Integer
     digits = 1 if digits < 1
@@ -60,12 +106,25 @@ class Problems < Command
     low = (10 ** (digits - 1))
     high = ((10 ** digits) - 1)
 
-    rand(low..high)
+    number = rand(low..high)
+
+    number = -number if rand(1..100) <= Normalize.from_string(self[:negative])
+
+    number = number.to_f if self[:float]
+
+    number
   end
 
+  # Generates a number of math problems. 
+  # If :send_directory is an extant file, it will write the questions to that file after confirming with the user.
+  # Otherwise, it will print the questions to the screen.
   def run
-    if self[:lower_digits].zero?
-      self[:lower_digits] = self[:digits]
+    output = []
+
+    sending = File.exist?(self[:send_directory].to_s)
+
+    if self[:operator_digits].zero?
+      self[:operator_digits] = self[:digits]
     end
 
     self[:active_mode] = if self[:input_modes].empty?
@@ -83,7 +142,20 @@ class Problems < Command
                 end
 
     questions.times do 
-      generate_problem
+      self[:active_mode] = OPERATIONS.keys[0...-1].sample if mode == :mixed
+      self[:active_mode] = :modulo if self[:active_mode] == :division && self[:modulo]
+
+      if sending
+        output << generate_problem
+      else
+        puts generate_problem
+      end
+    end
+
+    if sending
+      return unless confirm? "Are you sure you want to overwrite #{self[:send_directory]}?"
+      IO.write self[:send_directory], output.join("\n") 
+      puts "Sending examples to #{self[:send_directory]}"
     end
   end
 end
